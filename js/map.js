6967621cb340124b3795052781b65b47ea120d73
Helen V. Holmes <helen.v.holmes@gmail.com>
2013-10-18 10:46:21 -0400

    I don't know what I'm doing

--- js/map.js
+++ js/map.js
@@ -1,15 +1,4 @@
-// function initialize() {
-//   var mapOptions = {
-//     zoom: 8,
-//     center: new google.maps.LatLng(-34.397, 150.644),
-//      // disableDefaultUI: true,
-//      // mapTypeControl: false,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-//   }
-//   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-// }
-
-// Initialize the map.
+// Google Maps Settings. 
 
 var map,
     userLoc,
@@ -127,5 +116,7 @@ function loadScript() {
 }
 
 window.onload = loadScript;
+
+google.maps.event.trigger(map, 'resize');
 
 // google.maps.event.addDomListener(window, 'load', initialize);
