79086a37cdf3b13a19f8d63aef25be7a1a75babc
Helen V. Holmes <helen.v.holmes@gmail.com>
2013-10-21 01:42:09 -0400

    I'm changing colors

--- js/inits.js
+++ js/inits.js
@@ -15,7 +15,18 @@ $(document).ready(function(){
   });
 
   // ChartJS data
-  var data = [
+  var experienceData = [
+  {
+    value: 35,
+    color:"#F7464A"
+  }, 
+  {
+    value: 25,
+    color: "#D4CCC5"
+  }
+  ]
+
+  var placesData = [
   {
     value: 35,
     color:"#F7464A"
@@ -26,25 +37,9 @@ $(document).ready(function(){
   }
   ]
   var ctx = document.getElementById("experiences").getContext("2d");
-  var myNewChart = new Chart(ctx).Doughnut(data);
+  var myNewChart = new Chart(ctx).Doughnut(experienceData);
 
-
-  var data = {
-  labels : ["January","February","March","April","May","June","July"],
-  datasets : [
-    {
-      fillColor : "rgba(220,220,220,0.5)",
-      strokeColor : "rgba(220,220,220,1)",
-      data : [65,59,90,81,56,55,40]
-    },
-    {
-      fillColor : "rgba(151,187,205,0.5)",
-      strokeColor : "rgba(151,187,205,1)",
-      data : [28,48,40,19,96,27,100]
-    }
-  ]
-  }
-  var bar = document.getElementById("placesBeen").getContext("2d");
-  new Chart(bar).Bar(data);
+  var ctx = document.getElementById("placesBeen").getContext("2d");
+  var myNewChart = new Chart(ctx).Doughnut(placesData);
 
 });
