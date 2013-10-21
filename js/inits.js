373aa6f3da28f5b371ee2fdc7d306f2ca6e67de2
Helen V. Holmes <helen.v.holmes@gmail.com>
2013-10-20 23:13:57 -0400

    Seeing animation on phone

--- js/inits.js
+++ js/inits.js
@@ -14,39 +14,6 @@ $(document).ready(function(){
     $('.sidebar').hide("slide", 25);
   });
 
-  // Donut options
-  Doughnut.defaults = {
-    //Boolean - Whether we should show a stroke on each segment
-    segmentShowStroke : true,
-    
-    //String - The colour of each segment stroke
-    segmentStrokeColor : "#fff",
-    
-    //Number - The width of each segment stroke
-    segmentStrokeWidth : 2,
-    
-    //The percentage of the chart that we cut out of the middle.
-    percentageInnerCutout : 50,
-    
-    //Boolean - Whether we should animate the chart 
-    animation : true,
-    
-    //Number - Amount of animation steps
-    animationSteps : 100,
-    
-    //String - Animation easing effect
-    animationEasing : "easeOutBounce",
-    
-    //Boolean - Whether we animate the rotation of the Doughnut
-    animateRotate : true,
-
-    //Boolean - Whether we animate scaling the Doughnut from the centre
-    animateScale : false,
-    
-    //Function - Will fire on animation completion.
-    onAnimationComplete : null
-  }
-
   // ChartJS data
   var data = [
   {
@@ -71,16 +38,7 @@ $(document).ready(function(){
   }
 
   ]
-
-  // Chart js stuff
   var ctx = document.getElementById("milesWalked").getContext("2d");
-  
-  var myNewChart = new Chart(ctx).PolarArea(data);
-
-  var ctx = $("#milesWalked").get(0).getContext("2d");
-  //This will get the first returned node in the jQuery collection.
-  var myNewChart = new Chart(ctx);
-
-  new Chart(ctx).Doughnut(data,Doughnut);
+  var myNewChart = new Chart(ctx).Doughnut(data);
 
 });
